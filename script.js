0fc2c89712317f1cf88947097650747aade413da
brian-villa <brian.rj99@yahoo.com.br>
2023-11-27 12:44:39 +0000

    carousel done

---
+++ script.js
@@ -0,0 +1,23 @@
+/*carousel slider*/
+
+let radios = document.querySelector(".manual-btn")
+
+let count = 1
+
+document.getElementById("radio1").checked = true
+
+setInterval(() => {
+    nextImg()
+}, 5000)
+
+const nextImg = () => {
+    count++
+
+    if (count > 3) {
+        count = 1
+    }
+
+    document.getElementById("radio"+count).checked = true
+}
+
+
